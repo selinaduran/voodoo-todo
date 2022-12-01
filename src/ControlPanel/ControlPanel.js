72637552a7459c9a748bcec56b26056a7188fd35
selinaduran <selinad177@gmail.com>
2022-12-01 12:45:48 -0600

    used map to render multiple ButtonControl components

--- src/ControlPanel/ControlPanel.js
+++ src/ControlPanel/ControlPanel.js
@@ -1,41 +1,43 @@
-import ButtonControl from '../ButtonControl';
-import { actionOptions, flavorOptions, actions } from '../const';
-import './ControlPanel.css';
+import ButtonControl from "../ButtonControl";
+import { actionOptions, flavorOptions, actions } from "../const";
+import "./ControlPanel.css";
 
-const ControlPanel = ({ selectedAction, handleActionClick, selectedType, handleTypeClick }) => {
+const ControlPanel = ({
+  selectedAction,
+  handleActionClick,
+  selectedType,
+  handleTypeClick,
+}) => {
   const disableFlavorOptions =
-    selectedAction !== actions.add.action && selectedAction !== actions.flavor.action;
+    selectedAction !== actions.add.action &&
+    selectedAction !== actions.flavor.action;
 
   return (
     <>
-      <div className='panel'>
-        {
-          // This return statement will be returned when `actionOptions`
-          // are mapped
-          // return (
-          //   <ButtonControl
-          //     key={option.name}
-          //     selected={selectedAction}
-          //     option={option}
-          //     onClick={handleActionClick}
-          //   />
-          // );
-        }
+      <div className="panel">
+        {actionOptions.map((option) => {
+          return (
+            <ButtonControl
+              key={option.name}
+              selected={selectedAction}
+              option={option}
+              onClick={handleActionClick}
+            />
+          );
+        })}
       </div>
-      <div className='panel'>
-        {
-          // This return statement will be returned when `flavorOptions`
-          // are mapped
-          // return (
-          //   <ButtonControl
-          //     key={option.name}
-          //     selected={selectedType}
-          //     option={option}
-          //     onClick={handleTypeClick}
-          //     disabled={true} //'ReplaceWithCorrectValue'
-          //   />
-          // );
-        }
+      <div className="panel">
+        {flavorOptions.map((option) => {
+          return (
+            <ButtonControl
+              key={option.name}
+              selected={selectedType}
+              option={option}
+              onClick={handleTypeClick}
+              disabled={true} //'ReplaceWithCorrectValue'
+            />
+          );
+        })}
       </div>
     </>
   );
